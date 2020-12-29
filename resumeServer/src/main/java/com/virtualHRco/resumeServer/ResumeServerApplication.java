8f5a10f53e4758f473c8537d662dac174f590eab
BLoveless <bloveless@talentpath.com>
2020-12-29 14:08:50 -0500

    boiler plate java spring boot

---
+++ resumeServer/src/main/java/com/virtualHRco/resumeServer/ResumeServerApplication.java
@@ -0,0 +1,13 @@
+package com.virtualHRco.resumeServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeServerApplication.class, args);
+	}
+
+}
---
+++ resumeServer/src/test/java/com/virtualHRco/resumeServer/ResumeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtualHRco.resumeServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
